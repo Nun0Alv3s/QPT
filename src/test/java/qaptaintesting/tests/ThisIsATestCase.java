e9edac0153a2e80bcf842afc8c116a5e8e122a8e
Nuno Alves <nuno.alves@aptoide.com>
2026-03-24 00:25:31 +0000

    feat(test): integrate page object

--- src/test/java/qaptaintesting/tests/ThisIsATestCase.java
+++ src/test/java/qaptaintesting/tests/ThisIsATestCase.java
@@ -6,6 +6,8 @@ import org.testng.annotations.AfterTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import qaptaintesting.pageobjects.LetcodeInTestPageObject;
+
 public class ThisIsATestCase {
 
     protected WebDriver driver = new ChromeDriver();
@@ -15,6 +17,9 @@ public class ThisIsATestCase {
         // Starting URL can be set here when provided, for example:
         // driver.get("http://example.com");
         driver.get("https://letcode.in/test");
+
+        LetcodeInTestPageObject letcodeInTestPageObject = new LetcodeInTestPageObject(driver);
+        letcodeInTestPageObject.clickFooterFirstLink();
     }
 
     @AfterTest
