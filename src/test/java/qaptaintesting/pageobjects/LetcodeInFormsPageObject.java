05e04899ea09b1e6e5e2e9a2684863371da349c1
Nuno Alves <nuno.alves@aptoide.com>
2026-03-24 00:26:11 +0000

    feat(test): add page objects

--- src/test/java/qaptaintesting/pageobjects/LetcodeInFormsPageObject.java
+++ src/test/java/qaptaintesting/pageobjects/LetcodeInFormsPageObject.java
@@ -15,6 +15,9 @@ public class LetcodeInFormsPageObject {
     @FindBy(xpath = "//*[@id='lasttname']")
     protected WebElement lastNameInput;
 
+    @FindBy(xpath = "//*[@id='email']")
+    protected WebElement emailInputField;
+
     public LetcodeInFormsPageObject(WebDriver pDriver) {
         driver = pDriver;
         PageFactory.initElements(driver, this);
@@ -40,5 +43,15 @@ public class LetcodeInFormsPageObject {
     public void fillLastName(String value) {
         lastNameInput.clear();
         lastNameInput.sendKeys(value);
+    }
+
+    /**
+     * Fills the email input field with the specified value.
+     *
+     * @param email the email address to enter (e.g., "user9384@example-test.com")
+     */
+    public void fillEmail(String email) {
+        emailInputField.clear();
+        emailInputField.sendKeys(email);
     }
 }
